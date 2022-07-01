560cdd0c0f0b1a7c9d12f3db3570463105fbb8ce
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-06-30 21:24:25 -0400

    add function to change task text areas color based on time of day for past, present and future

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,24 @@ var datetime = moment().format('LLL');
 $("#local-date").append(datetime);
 
 // for past - present - future time blocks for colored rows
-
-
-
-
+var currentTime = moment().hour();
+var taskText = $(".task-text");
+// function to add and remove color depending on time of day for past - present - future tasks
+taskText.each(function () {
+    // using parseInt method to take textarea id to compare to use as integer for compairison to currentTime hour
+    let newTask = parseInt($(this).attr('id'));
+        
+        // if statement for row associated hour setting to if equals current hour then adding the class of present
+        if (newTask === currentTime) {
+            $(this).addClass("present");
+        // else if statement for row associated hour setting to if less than current hour then adding the class of past
+        } else if (newTask < currentTime) {
+            $(this).addClass("past");
+        // else if statement for row associated hour setting to if greater than current hour then adding the class of future
+        } else if (newTask > currentTime) {
+            $(this).addClass("future");
+        // else statement    
+        }  else{
+            $(this).addClass("white");
+        }
+});
