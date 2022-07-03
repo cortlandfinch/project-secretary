79953e5476cb291b9dd89352b34694379a31f53b
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-07-02 22:23:06 -0400

    removing unused button ID and add descriptions to JS file

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
 // for saveBtn to save text area on click
 $('.saveBtn').click(function() {
+    // using jquery siblings of textarea to get value user types in
     var meeting = $(this).siblings(".description").val();
+    // using jquery parent to get id associated with the row for that time slot
     var time = $(this).parent().attr("id");
+    // using localStorage to set the time and meeting to user local storage
     localStorage.setItem(time, meeting);
 })
 
+// using row ID and class description to get the value stored in user local storage in text area on page refresh
 $("#8 .description").val(localStorage.getItem('8'));
 $("#9 .description").val(localStorage.getItem('9'));
 $("#10 .description").val(localStorage.getItem('10'));
@@ -34,7 +38,7 @@ taskText.each(function () {
         // else if statement for row associated hour setting to if less than current hour then adding the class of past
         } else if (newTask < currentTime) {
             $(this).addClass("past");
-        // else if statement for row associated hour setting to if greater than current hour then adding the class of future
+        // else statement for rows to have future class
         } else {
             $(this).addClass("future");
         } 
