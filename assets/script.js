d1148980de221cdb23099f9562ecc3035afb5a00
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-07-02 22:13:37 -0400

    add saveBtn function and localstorage setItem and getItem on refresh

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,32 @@
+// for saveBtn to save text area on click
+$('.saveBtn').click(function() {
+    var meeting = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, meeting);
+})
+
+$("#8 .description").val(localStorage.getItem('8'));
+$("#9 .description").val(localStorage.getItem('9'));
+$("#10 .description").val(localStorage.getItem('10'));
+$("#11 .description").val(localStorage.getItem('11'));
+$("#12 .description").val(localStorage.getItem('12'));
+$("#13 .description").val(localStorage.getItem('13'));
+$("#14 .description").val(localStorage.getItem('14'));
+$("#15 .description").val(localStorage.getItem('15'));
+$("#16 .description").val(localStorage.getItem('16'));
+$("#17 .description").val(localStorage.getItem('17'));
+
 // displays current date and time in header
 var datetime = moment().format('LLL');
 $("#local-date").append(datetime);
 
 // for past - present - future time blocks for colored rows
 var currentTime = moment().hour();
-var taskText = $(".task-text");
+var taskText = $(".row");
 // function to add and remove color depending on time of day for past - present - future tasks
 taskText.each(function () {
     // using parseInt method to take textarea id to compare to use as integer for compairison to currentTime hour
-    let newTask = parseInt($(this).attr('id'));
+    let newTask = parseInt($(this).attr("id"));
         
         // if statement for row associated hour setting to if equals current hour then adding the class of present
         if (newTask === currentTime) {
@@ -17,14 +35,7 @@ taskText.each(function () {
         } else if (newTask < currentTime) {
             $(this).addClass("past");
         // else if statement for row associated hour setting to if greater than current hour then adding the class of future
-        } else if (newTask > currentTime) {
+        } else {
             $(this).addClass("future");
-        // else if statement for rows to default to white when not in current business hours
-        } else if((currentTime>= 18) || (currentTime<= 7)) {
-            $(this).addClass("white");
-        } else{
-            $(this).addClass("white");
-        }
+        } 
 });
-
-// for saveBtn to save text area on click
